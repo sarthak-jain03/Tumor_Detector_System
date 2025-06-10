a6297404fa6f6d7e75404716f0504fc4df53f847
Sarthak <sarthakjain4452@gmail.com>
2025-06-10 13:10:15 +0530

    Final changes

--- main.py
+++ main.py
@@ -75,5 +75,5 @@ def index():
 def get_uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=10000)
